ce71414fb5ce349eecebba5b840a1f2adb4a042f
Ramit Surana <ramitsurana@gmail.com>
2015-08-20 14:53:43 +0530

    added provisioning for chef-client for aws

--- configure.sh
+++ configure.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 yum upgrade -f;
 yum install git -y;
+
+ #If Chef is not installed
+curl -L https://www.opscode.com/chef/install.sh \
+
+rm -r ../vendor/cookbooks
+berks	 install --path	 ../vendor/cookbooks
+packer build \
+  ‐var  "account_id=$AWS_ACCOUNT_ID" \
+  ‐var  "aws_access_key_id=$AWS_ACCESS_KEY_ID" \
+  ‐var  "aws_secret_key=$AWS_SECRET_ACCESS_KEY"	 \	
+  ‐var  "x509_cert_path=$AWS_X509_CERT_PATH" \	 	
+  ‐var  "x509_key_path=$AWS_X509_KEY_PATH" \	 	 
+  ‐var	"s3_bucket=chef-packer‐bucket" \	 	
+  system-build.json
